a786e33c3322ffa779d7ba1c64f5faee42d730fe
David <david_zaoui@yahoo.fr>
2012-12-23 22:50:27 +0100

    game

---
+++ src/com/david/framework/impl/TouchHandler.java
@@ -0,0 +1,19 @@
+package com.david.framework.impl;
+
+import java.util.List;
+
+import android.view.View.OnTouchListener;
+
+import com.david.framework.Input.TouchEvent;
+
+public interface TouchHandler extends OnTouchListener {
+	
+	public boolean isTouchDown(int pointer);
+	
+	public int getTouchX(int pointer);
+	
+	public int getTouchY(int pointer);
+	
+	public List<TouchEvent> getTouchEvents();
+
+}
