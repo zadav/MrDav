02a654fbe48571ae7de3aa70ee710b8d7aca5fb9
David <david_zaoui@yahoo.fr>
2012-12-23 15:54:09 +0100

    first

---
+++ src/com/david/framework/Pixmap.java
@@ -0,0 +1,13 @@
+package com.david.framework;
+
+import com.david.framework.Graphics.PixmapFormat;
+
+public interface Pixmap {
+    public int getWidth();
+
+    public int getHeight();
+
+    public PixmapFormat getFormat();
+
+    public void dispose();
+}
---
+++ src/com/david/framework/Sound.java
@@ -0,0 +1,9 @@
+package com.david.framework;
+
+public interface Sound {
+	
+	    public void play(float volume);
+
+	    public void dispose();
+
+}
